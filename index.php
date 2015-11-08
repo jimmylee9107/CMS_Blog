637f16d49eeeadac233e4d1394929c73802b9415
jl578@duke.edu <jl578@duke.edu>
2015-11-08 00:36:39 -0500

    ad

--- index.php
+++ index.php
@@ -15,13 +15,14 @@
                 $query = "SELECT * FROM posts ";
                 $result = mysqli_query($connection, $query);
                 while($row = mysqli_fetch_assoc($result)) {
+                    $post_id = $row['post_id'];
                     $post_title = $row['post_title'];
                     $post_author = $row['post_author'];
                     $post_date = $row['post_date'];
                     $post_image = $row['post_image'];
                     $post_content = $row['post_content'];
 
-                    ?>
+                ?>
                  <h1 class="page-header">
                     Page Heading
                     <small>Secondary Text</small>
@@ -29,7 +30,7 @@
 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#"><?php echo $post_title;?></a>
+                    <a href="post.php?p_id=<?php echo $post_id;?>"> <?php echo $post_title?></a>
                 </h2>
                 <p class="lead">
                     by <a href="index.php"><?php echo $post_author;?></a>
