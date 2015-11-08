e486f2ed616e90fdf7841de8355f384c10de9be8
jl578@duke.edu <jl578@duke.edu>
2015-11-07 19:28:17 -0500

    update category

--- admin/categories.php
+++ admin/categories.php
@@ -67,35 +67,42 @@
                                <label for="cat-title">Edit Category</label>
 <?php
         if (isset($_GET['edit'])) {
-            $cat_id = $_GET['edit'];
-            $query = "SELECT * FROM categories WHERE cat_id = {$cat_id}";
+            $the_cat_id = $_GET['edit'];
+            $query = "SELECT * FROM categories WHERE cat_id = {$the_cat_id}";
             $select_categories_id = mysqli_query($connection, $query);
             while($row = mysqli_fetch_assoc($select_categories_id)) {
-                $cat_id = $row['cat_id'];
-                $cat_title = $row['cat_title'];
+                $the_cat_id = $row['cat_id'];
+                $the_cat_title = $row['cat_title'];
                 ?>
 
 
-                <input value="<?php if(isset($cat_title)) {echo $cat_title;} ?>" type="text" class="form-control" name="cat_title">
+                <input value="<?php if(isset($the_cat_title)) {echo $the_cat_title;} ?>" type="text" class="form-control" name="cat_title">
 
             <?php
-                }
+                }} ?>
 
-
-
-
+<?php
+        if (isset($_POST['update_category'])) {
+            $the_cat_title = $_POST['cat_title'];
+            if ($the_cat_title == "" || empty($the_cat_title)) {
+                echo "You can not update an empty category";
+            }
+            $query = "UPDATE categories SET cat_title = ";
+            $query .= "'{$the_cat_title}' ";
+            $query .= "WHERE cat_id = {$the_cat_id}";
+            $update_category = mysqli_query($connection, $query);
+            if (!$update_category) {
+                die("Query Failed" . mysqli_error($connection));
+            }
         }
 
-
-
-                                ?>
-
+?>
 
 
 <!--                                <input type="text" class="form-control" name="cat_title">    -->
                             </div>
                             <div class="form-group">
-                                <input class="btn btn-primary" type="submit" name="submit" value="Update Category">
+                                <input class="btn btn-primary" type="submit" name="update_category" value="Update Category">
                             </div>
 
 
